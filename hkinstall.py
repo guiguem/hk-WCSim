b29ee0b1f31b70cf3cfd33f80bb1f2ea3495096f
Mathieu Guigue <mgguigue@gmail.com>
2022-09-15 19:26:05 +0200

    Test commit

--- hkinstall.py
+++ hkinstall.py
@@ -15,7 +15,7 @@ class WCSim(CMake):
         self._git_url = "git@github.com:guiguem/WCSim.git"
         # self._git_branch = "master"
         # self._git_tag = "v4.3.482"
-        # self._git_clone_dir = "src"
+        self._git_clone_dir = "src"
         self._cmakelist_path = "src"
         self._depends_on = {"ROOT": "*", "Geant4": "*"}
         self._cmake_options = {
