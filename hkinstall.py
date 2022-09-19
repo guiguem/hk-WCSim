762b582a9b5a2bcf810964e9ae56e927eb29ecb1
Mathieu Guigue <mgguigue@gmail.com>
2022-09-19 15:47:07 +0200

    Cleanup

--- hkinstall.py
+++ hkinstall.py
@@ -11,16 +11,13 @@ class WCSim(CMake):
 
         self._package_name = "WCSim"
         self._package_version = "v1.10.0"
-        # self._download_url = "https://github.com/WCSim/WCSim/archive/refs/tags/v1.10.0.zip"
-        self._git_url = "git@github.com:guiguem/WCSim.git"
-        # self._git_branch = "master"
-        self._git_tag = "test_v1"
+        self._git_url = "git@github.com:WCSim/WCSim.git"
+        self._git_branch = "develop"
+        # self._git_tag = "test_v1"
         self._git_clone_dir = "src"
         self._cmakelist_path = "src"
         self._depends_on = {"ROOT": "*", "Geant4": "*"}
         self._cmake_options = {
             "CMAKE_CXX_STANDARD": "14",
-            # "Geant4_DIR": "/Users/mguigue/Work/T2K/HK/Software/newSystem/hkpilot/../Geant4/install-Darwin_arm64-gcc_13.1.6-python_3.9.13"
         }
 
-        # self._path = os.path.relpath(inspect.getfile(self.__class__))
